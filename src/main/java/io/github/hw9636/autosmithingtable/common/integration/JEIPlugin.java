3f7be2584f95fb371a035a2d4b6fc02ceb4b5557
9636Dev <9636dev@gmail.com>
2023-05-09 12:18:33 +0200

    Changed Licensing

--- src/main/java/io/github/hw9636/autosmithingtable/common/integration/JEIPlugin.java
+++ src/main/java/io/github/hw9636/autosmithingtable/common/integration/JEIPlugin.java
@@ -1,3 +1,19 @@
+/*
+    This file is part of 9636Dev's AutoSmithingTableMod.
+
+    AutoSmithingTableMod is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    AutoSmithingTableMod is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with AutoSmithingTableMod.  If not, see <https://www.gnu.org/licenses/>.
+*/
 package io.github.hw9636.autosmithingtable.common.integration;
 
 import io.github.hw9636.autosmithingtable.AutoSmithingTableMod;
